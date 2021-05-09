5b60c59a91f3f2bf12e60910017ffc1b6b362855
Fernando A. P. Gomes <fapg@nsk.pt>
2021-05-09 11:37:41 +0100

    sometimes, the publish stop working. correct this bug.

--- owl.py
+++ owl.py
@@ -2,20 +2,26 @@ import paho.mqtt.client as mqttClient
 import socket
 import struct
 import json
+import time
 import configparser
 from xml.etree import ElementTree
 import syslog
 
 # for debugging only
 debug = 0
+Connected = 0
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         syslog.syslog(syslog.LOG_INFO, "Connected to broker with the result code: " + str(rc))
         global Connected                #Use global variable
-        Connected = True                #Signal connection 
+        Connected = 1                   #Signal connection 
     else:
-        syslog.syslog(syslog.LOG_INFO, "Connected failed with the result code: " + str(rc))
+        syslog.syslog(syslog.LOG_INFO, "Connected to broker failed with the result code: " + str(rc))
+
+def on_disconnect(client, userdata, rc):
+   global Connected
+   Connected = 0
 
 def on_publish(client, userdata, result):             #create function for callback
     if debug :
@@ -38,9 +44,11 @@ broker_password = config['mqtt']['password']
 
 client = mqttClient.Client("owl2mqtt client")
 client.username_pw_set(broker_username, password=broker_password)
-client.on_connect= on_connect
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
 client.on_publish = on_publish
 client.connect(broker_address, port=broker_port)
+time.sleep(5)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -49,49 +57,57 @@ mreq = struct.pack("=4sl", socket.inet_aton(OWL_GROUP), socket.INADDR_ANY)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 while True:
-    # Collect the XML multicast message
-    xml, addr = sock.recvfrom(1024)
-    # Parse the XML string
-    root = ElementTree.fromstring(xml)
-
-    if root.tag == 'electricity' or root.tag == 'solar':
+    client.loop()
+    syslog.syslog(syslog.LOG_INFO, "Connected: " + str(Connected))
+    time.sleep(0.5)
     
-        timestamp_value = 0
-        timestamp = root.find('timestamp')
-        if timestamp is not None:
-            timestamp_value = int(timestamp.text)
-    
-        battery_value = 0
-        battery = root.find('battery')
-        if battery is not None:
-            battery_value = battery.attrib["level"]
+    if Connected == 1:
+        # Collect the XML multicast message
+        xml, addr = sock.recvfrom(1024)
+        # Parse the XML string
+        root = ElementTree.fromstring(xml)
 
-        signal_rssi_value = 0
-        signal_lqi_value = 0
-        signal = root.find('signal')
-        if signal is not None:
-            signal_rssi_value = signal.attrib["rssi"]
-            signal_lqi_value = signal.attrib["lqi"]
+        if root.tag == 'electricity' or root.tag == 'solar':
+        
+            timestamp_value = 0
+            timestamp = root.find('timestamp')
+            if timestamp is not None:
+                timestamp_value = int(timestamp.text)
+        
+            battery_value = 0
+            battery = root.find('battery')
+            if battery is not None:
+                battery_value = battery.attrib["level"]
 
-        syslog.syslog(syslog.LOG_INFO, "reading info, timestamp: " + str(timestamp_value) + ", battery: " + str(battery_value))
-        client.publish("owl/"+root.tag+"/timestamp", timestamp_value)
-        client.publish("owl/"+root.tag+"/battery", battery_value)
-        client.publish("owl/"+root.tag+"/rssi", signal_rssi_value)
-        client.publish("owl/"+root.tag+"/lqi", signal_lqi_value)
+            signal_rssi_value = 0
+            signal_lqi_value = 0
+            signal = root.find('signal')
+            if signal is not None:
+                signal_rssi_value = signal.attrib["rssi"]
+                signal_lqi_value = signal.attrib["lqi"]
 
-        for chan in root.iter('chan'):
-            chan_value = 0
-            chan_value = chan.attrib["id"]
+            syslog.syslog(syslog.LOG_INFO, "reading info, timestamp: " + str(timestamp_value) + ", battery: " + str(battery_value))
+            client.publish("owl/"+root.tag+"/timestamp", timestamp_value)
+            client.publish("owl/"+root.tag+"/battery", battery_value)
+            client.publish("owl/"+root.tag+"/rssi", signal_rssi_value)
+            client.publish("owl/"+root.tag+"/lqi", signal_lqi_value)
 
-            current_value = 0.0
-            current = chan.find('curr')
-            if current is not None:
-                current_value = float(current.text)
+            for chan in root.iter('chan'):
+                chan_value = 0
+                chan_value = chan.attrib["id"]
 
-            day_value = 0.0
-            day = chan.find('day')
-            if day is not None:
-                day_value = float(day.text)
+                current_value = 0.0
+                current = chan.find('curr')
+                if current is not None:
+                    current_value = float(current.text)
 
-            client.publish("owl/"+root.tag+"/channel"+chan_value, current_value)
-            client.publish("owl/"+root.tag+"/daychannel"+chan_value, day_value)
+                day_value = 0.0
+                day = chan.find('day')
+                if day is not None:
+                    day_value = float(day.text)
+
+                client.publish("owl/"+root.tag+"/channel"+chan_value, current_value)
+                client.publish("owl/"+root.tag+"/daychannel"+chan_value, day_value)
+    else:
+        client.connect(broker_address, port=broker_port)
+        time.sleep(5)
